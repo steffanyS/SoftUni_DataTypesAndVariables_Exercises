dfd1530bee19ad7184077f0abfd762ec91526f87
steffanyS <stefani.sabcheva@gmail.com>
2017-01-19 15:57:20 +0200

    1ex done

---
+++ 01_IntegerNumbers/01_IntegerNumbers/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_IntegerNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //-100
+            //128
+            //- 3540
+            //64876
+            //2147483648
+            //- 1141583228
+            //- 1223372036854775808
+
+            sbyte firstNumber = -100;
+            byte secondNumber = 128;
+            short thirdNumber = -3540;
+            int fourthNumber = 64876;
+            uint fifthNumber = 2147483648;
+            int sixthNumber = -1141583228;
+            long seventhNumber = -1223372036854775808;
+
+            Console.WriteLine(firstNumber);
+            Console.WriteLine(secondNumber);
+            Console.WriteLine(thirdNumber);
+            Console.WriteLine(fourthNumber);
+            Console.WriteLine(fifthNumber);
+            Console.WriteLine(sixthNumber);
+            Console.WriteLine(seventhNumber);
+
+        }
+    }
+}
